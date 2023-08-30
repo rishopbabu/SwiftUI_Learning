aa0aa9a0d2690973fbc3298a3a8a440edacd26b7
Rishop Babu <rishopbabu22@gmail.com>
2023-08-30 17:15:03 +0530

    Initial Commit

---
+++ SwiftUI_Learning/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI_Learning
+//
+//  Created by Mac-OBS-51 on 30/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Learning/SwiftUI_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LearningApp.swift
+//  SwiftUI_Learning
+//
+//  Created by Mac-OBS-51 on 30/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
