37677f0c44f39780d54456bfc044262bc9c2d752
Rishop Babu <rishopbabu22@gmail.com>
2023-08-30 18:27:22 +0530

    initial commit

--- SwiftUI_Learning/ContentView.swift
+++ SwiftUI_Learning/ContentView.swift
@@ -9,13 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack{
+            Color(uiColor: .systemMint)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("waterfalls")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                HStack{
+                    Text("Water Falls")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                    
+                    Spacer()
+                    
+                    VStack{
+                        HStack{
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+                        .foregroundColor(.yellow).font(.caption)
+                        
+                        Text("(Reviews 365)")
+                            .font(.body)
+                            .fontWeight(.light)
+                    }
+                }
+                
+                HStack{
+                    Text("This waterfall is in Coimbatore")
+                        .font(.body)
+                        .fontWeight(.medium)
+                    
+                    Spacer()
+                    
+                    HStack{
+                        Spacer()
+                        Image(systemName: "fork.knife")
+                        Image(systemName: "binoculars.fill")
+                    }
+                    .foregroundColor(.gray)
+                    .font(.caption)
+                }
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(15)
+                .shadow(radius: 30))
+            .padding()
+        }        
     }
 }
 
