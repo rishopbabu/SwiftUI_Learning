9534de64090306f57b6e510d0f7493edab64d51e
Rishop Babu <rishopbabu22@gmail.com>
2023-09-05 11:25:17 +0530

    - badges

---
+++ SwiftUI_Learning/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUI_Learning
+//
+//  Created by Mac-OBS-51 on 05/09/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
